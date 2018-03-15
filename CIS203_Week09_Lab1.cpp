645bed24e36cbd730aa7c4c3bd837e455e6f0e80
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-03-14 20:26:25 -0500

    week 9 lab

--- CIS203_Week09_Lab1.cpp
+++ CIS203_Week09_Lab1.cpp
@@ -12,3 +12,21 @@ int main(){
 	cout << "Firstname: " <<  name << " Lastname: " << copy << endl;
 
 }
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+
+//int main(){
+
+//string test= "Rockers";
+
+//cout << test << endl;
+//test = "Robert";
+//cout << test << endl;
+//cout << "The length of test is " << test.length() << endl;
+//cout << "The 3rd char of test is " << test[2] << endl;
+
+//for(int i=0; i<test.length(); i++){
+//	cout << "You spell Robert with a " << test[i] << endl;
+//	}
